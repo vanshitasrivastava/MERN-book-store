eba7e5e5e9fae9b2c459a9c0adf96b5a6a04c1da
vanshita <vanshi1113@gmail.com>
2025-03-01 18:57:56 +0530

    some changes

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,5 +1,5 @@
 import User from "../model/user.model.js";
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 export const signup=async (req,res) =>{
    try {
       const {fullname,email,password} =req.body;
